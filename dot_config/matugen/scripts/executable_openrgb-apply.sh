ed2b2eb19487c8aee662e7768cde4eebd49edae9
Arthur-Conti <arthurconti2003@gmail.com>
2026-04-17 14:33:58 -0300

    many many changes

---
+++ dot_config/matugen/scripts/executable_openrgb-apply.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# O Matugen vai injetar o HEX com hashtag aqui (ex: #FF5555)
+PRIMARY_COLOR="#004689"
+
+# Removemos a hashtag para o OpenRGB ler corretamente (fica: FF5555)
+CLEAN_HEX="${PRIMARY_COLOR//#/}"
+
+# Envia a cor para os LEDs
+openrgb -m direct -c "$CLEAN_HEX"
