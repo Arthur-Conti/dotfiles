335972e127ec3f18ee499bb7def864d5bc02a2a7
Arthur-Conti <arthurconti2003@gmail.com>
2026-04-17 14:34:06 -0300

    many many changes

--- dot_config/nvim/lua/matugen.lua
+++ dot_config/nvim/lua/matugen.lua
@@ -1,22 +1,22 @@
 return {
   setup = function()
     require('base16-colorscheme').setup({
-      base00 = "#16121a",
-      base01 = "#231e26",
-      base02 = "#2e2831",
-      base03 = "#4d4352",
-      base04 = "#988d9e",
-      base05 = "#eadfeb",
-      base06 = "#cfc2d5",
-      base07 = "#eadfeb",
+      base00 = "#111318",
+      base01 = "#1d2024",
+      base02 = "#282a2f",
+      base03 = "#424751",
+      base04 = "#8c919c",
+      base05 = "#e2e2e8",
+      base06 = "#c2c6d2",
+      base07 = "#e2e2e8",
       base08 = "#ffb4ab",
-      base09 = "#ffb0cf",
-      base0A = "#deb8fb",
-      base0B = "#e0b7ff",
-      base0C = "#6911a8",
-      base0D = "#e0b7ff",
-      base0E = "#deb8fb",
-      base0F = "#ffb0cf"
+      base09 = "#f1affe",
+      base0A = "#b6c7e8",
+      base0B = "#a8c8ff",
+      base0C = "#004689",
+      base0D = "#a8c8ff",
+      base0E = "#b6c7e8",
+      base0F = "#f1affe"
     })
   end
 }
